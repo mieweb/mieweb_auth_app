3854a05fd4c3d703fa769d0a7e0129e4296a3611
Anshul Abrol <abroa01@pfw.edu>
2025-01-18 21:42:15 -0500

    Send response from client based on user actions

--- send_notification.sh
+++ send_notification.sh
@@ -3,24 +3,26 @@
 curl -X POST http://localhost:3000/send-notification \
 -H "Content-Type: application/json" \
 -d '{
-    "appId": "505a9c017d3d833ad611b50bf58c6c2c",
+    "appId": "2c142ea8d3c29cdabbe9f6f2043389ac",
     "title": "MIE Sudo Security Alert",
-    "body": "Please review and respond to your pending MIE request in the app",
+    "body": "Please review and respond to your pending MIE request in the app.",
     "actions": [
     {
       "icon": "approve",
       "title": "Approve",
       "callback": "approve",
-      "foreground": true
+      "foreground": true,
+      "background": true
     },
     {
       "icon": "reject",
       "title": "Reject",
       "callback": "reject",
-      "foreground": true
+      "foreground": true,
+      "background": true
     }
   ]
-}' | (sleep 6; cat)
+}'
 
 
 # If you set the foreground property to true, the app will be brought to the front, if foreground is false then the callback is run without the app being brought to the foreground.
