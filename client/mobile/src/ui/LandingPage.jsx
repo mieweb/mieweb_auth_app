df74f935f8896125bfca24d8702caf357c937f23
Anshul Abrol <aabrol@MacBook-Y6YVQRW79C.local>
2025-06-20 15:43:20 -0400

    renaming the Msecure to MieAuth

--- client/mobile/src/ui/LandingPage.jsx
+++ client/mobile/src/ui/LandingPage.jsx
@@ -89,7 +89,7 @@ export const LandingPage = () => {
   return (
     <div className={`min-h-screen bg-gradient-to-br from-indigo-50 to-blue-100 dark:from-gray-900 dark:to-gray-800`}>
       <DashboardHeader
-        title="MSecure"
+        title="MieAuth"
         isDarkMode={isDarkMode}
         toggleDarkMode={toggleDarkMode}
         onRefresh={fetchNotificationHistory} // Use refetch from hook
